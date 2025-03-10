47da3df0b8cb5d50a7979b881bedec013ef68e39
Emerson Segnini de Santi <emersonsegninidesanti@gmail.com>
2025-03-10 17:24:40 -0300

    contact admin model, __str__, list_order

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+class ContactAdmin (admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone',)  
+
+admin.site.register(models.Contact, ContactAdmin)
