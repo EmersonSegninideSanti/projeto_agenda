4e4151a0e751b40260c81b25c929385d819f879e
Emerson Segnini de Santi <emersonsegninidesanti@gmail.com>
2025-04-14 17:26:05 -0300

    Created Model Category, AdminModel CategoryAdmin, foreignkey in Contact

--- contact/admin.py
+++ contact/admin.py
@@ -4,6 +4,10 @@ from contact import models
 # Register your models here.
 
 class ContactAdmin (admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone',)  
+    list_display = ('first_name', 'last_name', 'phone',)
+
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('name',)
 
 admin.site.register(models.Contact, ContactAdmin)
+admin.site.register(models.Category, CategoryAdmin)
--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,12 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50,)
+
+    def __str__(self):
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -18,6 +24,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, blank = True, null= True)
 
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
