64789b26266e905b0e84604befe79962aaeff450
Emerson Segnini de Santi <emersonsegninidesanti@gmail.com>
2025-05-27 18:09:07 -0300

    Added error 404 to contact_view

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from contact.models import Contact
-
+from django.http import Http404
 import django.conf
 
 # Create your views here.
@@ -15,7 +15,7 @@ def index (request, ):
 def contact_view (request, id_number):
     contact = Contact.objects.filter(id=id_number).first()
     if contact is None:
-        raise 
+        raise Http404
     
     print(django.conf.settings.DEBUG)
     return render(request, 'contact/contact.html', context= {
