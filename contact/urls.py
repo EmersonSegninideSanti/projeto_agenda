26de81c0ec787cf424d43494cb8e42ab4f259f15
Emerson Segnini de Santi <emersonsegninidesanti@gmail.com>
2025-06-10 18:17:21 -0300

    Header form leads to search page. Added a functional search page (Pesquisa apenas em first_name.)

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ from contact import views
 
 urlpatterns = [
     path( '', views.index, name= 'index'),
-    path( '<int:id_number>/', views.contact_view, name = 'contact',)
+    path( '<int:id_number>/', views.contact_view, name = 'contact',),
+    path( 'search/', views.search , name="search")
 ]
--- contact/views.py
+++ contact/views.py
@@ -12,6 +12,31 @@ def index (request, ):
         'contacts': contacts
     })
 
+def search(request, ):
+    # Código para procurar em qualquer campo
+    research_value = request.GET['q'].strip()
+    if research_value == '':
+        return index(request)
+    print(request.GET)
+    print(research_value)
+    # Aqui acontece uma query. QuerySetApi - field lookup.
+    contacts = Contact.objects.filter(first_name__icontains = research_value)
+    return render (request,'contact/index.html', context = {
+        'contacts': contacts
+    })
+    # Código simples para procurar em ids apenas.
+    # research_value = request.GET['q']
+    # print(request.GET)
+    # print(research_value)
+    # try: 
+    #     research_value = int(research_value)
+    # except:
+    #     return index(request)
+    # contacts = Contact.objects.filter(id = research_value)
+    # return render(request, 'contact/index.html', context= {
+    #     'contacts': contacts
+    # })
+
 def contact_view (request, id_number):
     contact = Contact.objects.filter(id=id_number).first()
     if contact is None:
